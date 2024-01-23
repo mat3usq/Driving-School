a1ac7d3e4c97cfbeb8cfbbe0e0ef83ca346ab85f
mat3usq <mmogielnicki123@gmail.com>
2024-01-23 16:48:51 +0100

    final version design

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar')
 	const navLinks = document.querySelectorAll('.nav-link')
 	const navBar = document.querySelector('.navbar-collapse')
+	const menuBars = document.getElementById('menuBars')
 
 	function addShadow() {
 		if (window.scrollY >= 20) nav.classList.add('shadow-bg')
@@ -9,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 	navLinks.forEach(link => link.addEventListener('click', () => navBar.classList.remove('show')))
-
 	window.addEventListener('scroll', addShadow)
+	menuBars.addEventListener('click', () => {
+		nav.classList.add('shadow-bg')
+	})
 })
